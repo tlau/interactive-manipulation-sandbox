ac371e3a91cefc33f5faf6624eb1d7056f435c18
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-15 19:53:53 -0300

    added a model for bif_action

---
+++ binfrastructure/django/world/migrations/0002_added_bif_action.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'BIFAction'
+        db.create_table('world_bifaction', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('params', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal('world', ['BIFAction'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'BIFAction'
+        db.delete_table('world_bifaction')
+
+
+    models = {
+        'world.bifaction': {
+            'Meta': {'object_name': 'BIFAction'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'params': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'world.bin': {
+            'Meta': {'object_name': 'Bin'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_empty': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'bins'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['world.BinLocation']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'pose_last_seen': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['world.Pose']", 'null': 'True'}),
+            'time_last_seen': ('django.db.models.fields.DateTimeField', [], {'null': 'True'})
+        },
+        'world.binlocation': {
+            'Meta': {'object_name': 'BinLocation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'pickup_dropoff_pose': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['world.Pose']", 'unique': 'True'})
+        },
+        'world.pose': {
+            'Meta': {'object_name': 'Pose'},
+            'angle': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'x': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'y': ('django.db.models.fields.FloatField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['world']
--- binfrastructure/django/world/models.py
+++ binfrastructure/django/world/models.py
@@ -47,3 +47,92 @@ class Bin(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+###############################################################################
+
+"""
+BIF Actions are modeled as a (name, params) pair, where the "name" is one of
+the defined in the API, identifying the function to call, and the "params" is a
+string of space separated values to be interpreted as the function paramter(s).
+
+Each BIF Action is expected to be able to build its arguments form the "params"
+string. As an example, consider a "go_to_pose" action receiving a params string
+like "3.0 -2.2 1.5". It should correspond to a call to
+
+   go_to_pose(Pose(x=3.0, y=-2.2, angle=1.5))
+
+The arguments() method of a BIFAction produces a dict instance with keys "name"
+and "arguments". The "name" is the API function to invoke; the "arguments" is a
+python object, ready to be passed to that function.
+
+It is to be noted that the "params" string is limited. This is so to simplify
+implementation. The maximum length is exposed as MAX_LENGTH_PARAMS_STRING.
+
+PARAMS DESERIALIZATION:
+
+  "...id_list" kind of params string is expected to be ID values (what every
+  type) separated by single-space characters.
+  Examples: "1 2 3" "'hi hola bonasera' 'chau bye adio'" "[1,2,3]"
+  It's turned into a list of those ID values.
+
+  "pose" kind of params string is expected to be three consecutive numeric
+  values separated by single-space characters. This are interpreted as "x", "y"
+  and "angle" values of a Pose instance.
+  It's turned into a (non-saved) Pose instance.
+
+  "text" kind of params string are considered to be a single string and
+  interpreted as-is.
+
+  "seconds" kind of params string are expected to be a single numeric value.
+  It's turned into a float.
+"""
+
+MAX_LENGTH_PARAMS_STRING = 200
+
+# Maps API call names to clien selected client BIF actions.
+# choice[0] : API call name
+# choice[1] : web UI name for the action.
+API_call_choices = (
+    ('pick_up_bin', 'pick_up_bin'),
+    ('pick_up_bin_from_location', 'pick_up_bin_from_location'),
+    ('drop_off_bin_at_location', 'drop_off_bin_at_location'),
+    ('go_to_pose', 'go_to_pose'),
+    ('speak', 'speak'),
+    ('wait', 'wait'),
+)
+
+
+class BIFAction(models.Model):
+
+    name = models.CharField(max_length=50, choices=API_call_choices)
+    params = models.CharField(max_length=MAX_LENGTH_PARAMS_STRING)
+
+    def argument(self):
+        """Translate the params string into arguments for the API call."""
+        if self.name not in [c[0] for c in API_call_choices]:
+            raise ValueError("bad API call name.")
+
+        if self.name in ['pick_up_bin',
+                         'pick_up_bin_from_location',
+                         'drop_of_bin_at_location']:
+            # string of single-space separated IDs.
+            return [ID for ID in self.params.split(' ')]
+        if self.name == 'go_to_pose':
+            # three values, single-space separated.
+            x, y, angle = self.params.split(' ')
+            return Pose(x=x, y=y, angle=angle)
+        if self.name == 'speak':
+            # a text string.
+            return self.params
+        if self.name == 'wait':
+            # a single numeric value.
+            seconds = float(self.params)
+            return seconds
+
+    def translate(self):
+        """Produce a dict() representation of this instance."""
+        return {
+            'name': self.name,
+            'arguments': self.argument()
+        }
