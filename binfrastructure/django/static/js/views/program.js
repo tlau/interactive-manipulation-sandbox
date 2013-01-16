fb716837dd4b449df9db223ad31ef835fb3dae89
Tessa Lau <tlau@willowgarage.com>
2013-01-16 13:35:31 -0300

    Started building views for each BINF_ACTION.

--- binfrastructure/django/static/js/views/program.js
+++ binfrastructure/django/static/js/views/program.js
@@ -2,12 +2,14 @@ define([
   'ember',
   'd3',
   'app',
+  'step',
   'text!templates/program.handlebars'
 ],
 function(
   Ember,
   d3,
   App,
+  Step,
   programHtml
 ) {
 
@@ -57,16 +59,41 @@ function(
     },
 
     addStep: function(newstep) {
-      this.program.push(newstep);
+      var index = this.program.push(newstep) - 1;
 
       // manually redraw the program; I can't get Ember observers to work
       this.drawProgram();
+      this.selectStep(index);
     },
 
     // Change which step is currently selected
     selectStep: function(index) {
       $('.selected').removeClass('selected');
       $('#step' + index).addClass('selected');
+
+      var step = this.program[index];
+      this.showPanel(step.type);
+    },
+
+    showPanel: function(steptype) {
+      // Change the right panel to display step parameters
+      if (steptype === null) {
+        // Show the actions panel
+        $('.selectedPanel').addClass('hidden');
+        $('.selectedPanel').removeClass('selectedPanel');
+        $('#actions').removeClass('hidden');
+      } else {
+        // Hide the actions panel and the current step panel (if any)
+        $('#actions').addClass('hidden');
+        $('.selectedPanel').addClass('hidden');
+        $('#' + steptype).removeClass('hidden');
+        $('#' + steptype).addClass('selectedPanel');
+      }
+    },
+
+    // Switch back to actions view
+    doneEditingStep: function(index) {
+      this.showPanel(null);
     },
   
     /* ---------------------------------------------------------------------- */
@@ -76,48 +103,70 @@ function(
       var locations = new Array("Elvio's office", "Julian's office", "the kitchen",
         "the Green Room", "the stockroom", "Esmi's office", "Kaijen's office", "the White Lab");
       var loc = locations[Math.floor(Math.random() * locations.length)];
-      this.addStep({
+      this.addStep(Step.create({
         'title': 'Pick up from ' + loc,
-        });
+        'type': 'pickup',
+        'location': loc,
+        }));
     },
 
     dropoff: function(evt) {
       var locations = new Array("Elvio's office", "Julian's office", "the kitchen",
         "the Green Room", "the stockroom", "Esmi's office", "Kaijen's office", "the White Lab");
       var loc = locations[Math.floor(Math.random() * locations.length)];
-      this.addStep({
+      this.addStep(Step.create({
         'title': 'Drop off at ' + loc,
-        });
+        'type': 'dropoff',
+        'location': loc,
+        }));
+    },
+
+    gotoPlace: function(evt) {
+      var locations = new Array("Elvio's office", "Julian's office", "the kitchen",
+        "the Green Room", "the stockroom", "Esmi's office", "Kaijen's office", "the White Lab");
+      var loc = locations[Math.floor(Math.random() * locations.length)];
+      this.addStep(Step.create({
+        'title': 'Take bin to ' + loc,
+        'type': 'goto',
+        'location': loc,
+        }));
     },
 
     speak: function(evt) {
-      this.addStep({
+      this.addStep(Step.create({
         'title': 'Say "Hello!"',
-        });
+        'type': 'speak',
+        'text': 'Hello!',
+        }));
     },
 
     wait: function(evt) {
       var num = (Math.floor(Math.random() * 29) + 1) * 10;
-      this.addStep({
+      this.addStep(Step.create({
         'title': 'Wait ' + num + ' seconds',
-        });
+        'type':'wait',
+        'duration': num,
+        }));
     },
 
     recharge: function(evt) {
-      this.addStep({
-        'title': 'Recharge batteries',
-        });
+      this.addStep(Step.create({
+        'title': 'Dock with charger',
+        'type':'recharge',
+        }));
     },
 
     /* ---------------------------------------------------------------------- */
     // Program execution
     runProgram: function(evt) {
-      console.log("Run program pressed");
-      alert("Not implemented yet!");
-    }
+      var ret = JSON.stringify(this.program.map(function (d) { return d.toAPI(); }));
+      // Send ret to the middleware layer here
+      console.log(ret);
+      alert("Sorry, not implemented yet!");
+    },
+
+
 
   });
 
 });
-
-
