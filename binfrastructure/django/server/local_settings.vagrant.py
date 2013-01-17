d33dd2c43517d3fe0fbfb1390232c960f31bd6d1
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-17 09:23:47 -0300

    got the remote database out of the vagrant installation.

--- binfrastructure/django/server/local_settings.vagrant.py
+++ binfrastructure/django/server/local_settings.vagrant.py
@@ -1,12 +1,12 @@
 # This is the local_settings.py file for use with Vagrant.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'layer2',  # Or path to database file if using sqlite3.
-        'USER': 'layer2remote',                        # Not used with sqlite3.
-        'PASSWORD': 'willow',                    # Not used with sqlite3.
-        'HOST': 'babylon1.willowgarage.com',     # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                        # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': '',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',     # Or path to database file if using sqlite3.
+        'USER': '',     # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '',     # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',     # Set to empty string for default. Not used with sqlite3.
     }
 }
 
