3ed7ecf88d0ed1766bbf4d9b3a5f6fbd397cf05d
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-17 09:22:54 -0300

    configured a dev logger for the backend.

--- binfrastructure/django/server/local_settings.local.py
+++ binfrastructure/django/server/local_settings.local.py
@@ -20,3 +20,36 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     '/home/vagrant/binfrastructure/django/templates/'
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'dev': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
